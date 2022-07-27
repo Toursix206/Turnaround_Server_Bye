768efde2cc7c58409ea7de121ceeff730172eb4f
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-27 17:42:46 +0900

    #11 [fix] 응답 형태 수정

--- src/main/java/com/toursix/turnaround/controller/auth/AuthController.java
+++ src/main/java/com/toursix/turnaround/controller/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.toursix.turnaround.controller.auth;
 
 import com.toursix.turnaround.common.dto.ApiResponse;
+import com.toursix.turnaround.common.success.SuccessCode;
 import com.toursix.turnaround.controller.auth.dto.request.LoginRequestDto;
 import com.toursix.turnaround.controller.auth.dto.response.LoginResponse;
 import com.toursix.turnaround.service.auth.AuthService;
@@ -32,12 +33,12 @@ public class AuthController {
         Long userId = authService.login(request.toServiceDto());
 
         TokenResponseDto tokenInfo = createTokenService.createTokenInfo(userId);
-        return ApiResponse.success(LoginResponse.of(userId, tokenInfo));
+        return ApiResponse.success(SuccessCode.LOGIN_SUCCESS, LoginResponse.of(userId, tokenInfo));
     }
 
     @ApiOperation("JWT 인증 - Access Token을 갱신합니다.")
     @PostMapping("/v1/auth/refresh")
     public ApiResponse<TokenResponseDto> reissue(@Valid @RequestBody TokenRequestDto request) {
-        return ApiResponse.success(createTokenService.reissueToken(request));
+        return ApiResponse.success(SuccessCode.REISSUE_TOKEN_SUCCESS, createTokenService.reissueToken(request));
     }
 }
