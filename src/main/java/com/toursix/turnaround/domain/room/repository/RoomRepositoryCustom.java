bfd8501c09b5514a6ba5b91162897be0dba03368
HyeokJoon Kong <orijoon98@gmail.com>
2022-08-11 23:51:01 +0900

    #33 [refactor] 마이페이지 조회 api 리팩토링

--- src/main/java/com/toursix/turnaround/domain/room/repository/RoomRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.toursix.turnaround.domain.room.repository;
-
-import com.toursix.turnaround.domain.room.Room;
-
-public interface RoomRepositoryCustom {
-
-    Room findRoomById(Long id);
-}
